9e06d4fea00166ced6d8daa9330486a900ad4ca5
Adham <admuaq@gmail.com>
2018-07-21 13:52:09 +0100

    Completed

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+require "pry"
+class Author
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+
+  def add_post_by_title(title)
+    #binding.pry
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
